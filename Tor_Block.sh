246e603f17a5b9d86e3f71943030f37a7d477f35
yitzyire <david.fitzpatrick51@gmail.com>
2017-02-27 11:22:58 -0800

    Update Tor_Block.sh

--- Tor_Block.sh
+++ Tor_Block.sh
@@ -11,5 +11,8 @@ sudo curl -O https://check.torproject.org/exit-addresses
 #extract the ip addressing within that file and pipe to a single clean file
 grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' exit-addresses > /home/admin2/tordiff/toraddresses.txt
 
+#flush the iptables (old entries)
+iptables -F
+
 #input line of IP address to IPtables then save
 for i in `cat toraddresses.txt`; do iptables -A INPUT -s $i -j DROP; done
