2567a8aae791454917a44caa2b101227da626f42
David Fitzpatrick <dfitzpatrick@riotgames.com>
2017-11-29 18:57:44 +0000

    updated script

--- Tor_Block.sh
+++ Tor_Block.sh
@@ -2,17 +2,18 @@
 #Automation - Blacklisting Tor addresses
 
 #Delete old piped download locations
-rm -rf /home/admin2/tordiff/exit-addresses
-rm -rf /home/admin2/tordiff/toraddresses.txt
+rm -rf /scripts/tor/addresses/exit-addresses
+rm -rf /scripts/tor/addresses/toraddresses
 
 #Pipe the contents to a exit-addresses file
 sudo curl -O https://check.torproject.org/exit-addresses
+mv /scripts/tor/exit-addresses /scripts/tor/addresses/exit-addresses
 
 #extract the ip addressing within that file and pipe to a single clean file
-grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' exit-addresses > /home/admin2/tordiff/toraddresses.txt
+grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /scripts/tor/addresses/exit-addresses > /scripts/tor/addresses/toraddresses
 
 #flush the iptables (old entries)
 iptables -F
 
 #input line of IP address to IPtables then save
-for i in `cat toraddresses.txt`; do iptables -A INPUT -s $i -j DROP; done
+for i in `cat /scripts/tor/addresses/toraddresses`; do iptables -A INPUT -s $i -j DROP; done
